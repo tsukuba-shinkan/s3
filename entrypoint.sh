a3c0a69d7ee68ce749a2de29eb5f246458947ea3
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-04-11 18:03:54 +0900

    heroku

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+PORT="${PORT:-8000}"
+echo "Running in $PORT"
 python3 download.py
 python3 gen_wordtable.py
 python3 gen_wordtable_event.py
 
-uvicorn app:app --host 0.0.0.0
+uvicorn app:app --host 0.0.0.0 --port $PORT
